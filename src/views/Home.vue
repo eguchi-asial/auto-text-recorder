63bbdd36e3945c1a50fbef3b4ee272f996983ca0
eguchi <eguchi@asial.co.jp>
2019-10-19 21:48:03 +0900

    基本機能実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,64 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="recognition">
+      <button class="record-btn" @click="record">{{ recordBtnValue }}</button>
+      <div>
+        <textarea class="recorded-text" v-model="recoredText" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-// @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+@Component({
+  components: {
+    HelloWorld
+  }
+})
 export default class Home extends Vue {
-  msg: string = "Hello";
+  msg: string = "こんちは！！";
+  recognizing: boolean = false;
+  recognition: any = null;
+  recordBtnValue = "録音";
+  recoredText: string = "";
+
+  async created() {
+    const { webkitSpeechRecognition } = window as any;
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = "ja-JP";
+    recognition.continuous = true;
+    recognition.onresult = await this.recognize;
+    this.recognition = recognition;
+  }
+
+  protected record() {
+    if (this.recognizing) {
+      this.recognition.stop();
+      this.recordBtnValue = "録音";
+    } else {
+      this.recognition.start();
+      this.recordBtnValue = "停止";
+    }
+    this.recognizing = !this.recognizing;
+  }
+
+  protected async recognize(e: SpeechRecognitionEvent) {
+    this.recoredText += `${e.results[e.results.length - 1][0].transcript}\n`;
+  }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  .recognition {
+    .recorded-text {
+      width: 300px;
+      height: 7rem;
+      line-height: 1.5em;
+    }
+  }
+}
+</style>
